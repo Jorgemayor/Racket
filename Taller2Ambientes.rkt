d784ea306227ae031f001fc3ad7c52774722a8d9
Odzen <49286935+Odzen@users.noreply.github.com>
2019-06-20 00:43:48 -0500

    Punto 2 resuelto con algunos ejemplos

--- Taller2Ambientes.rkt
+++ Taller2Ambientes.rkt
@@ -30,19 +30,16 @@
 (predecessor '(0 15)); (15 14)
 
 
-
-
-
 ;Codigo Cliente
-(define suma
+(define sumaBase
   (lambda (x y)
     (if (is-zero? x)
         y
-        (successor (suma (predecessor x) y)))))
+        (successor (sumaBase (predecessor x) y)))))
 
 ;pruebas
-(suma '(2 4)'(1)) ;(3 4)
-(suma '(15 1)'(3 2)); (2 4)
+(sumaBase '(2 4)'(1)) ;(3 4)
+(sumaBase '(15 1)'(3 2)); (2 4)
 
 
 
@@ -60,7 +57,7 @@
   (lambda (x y)
     (if (is-zero? x)
         (zero)
-        (suma (multiplicacion (predecessor x) y) y))
+        (sumaBase (multiplicacion (predecessor x) y) y))
     ))
 
 (multiplicacion '(0 1) '(2)); (0 2)
@@ -84,8 +81,72 @@
 ;Punto 2
 (define-datatype sumaAnidada sumaAnidada?
   (valorType (x number?))
-  (sumaType (randAnidada sumaAnidada?)
-            (ratorAnidada sumaAnidada?))
-  )
+  (sumaType (x number?) (body sumaAnidada?))
+ )
+
 ;Unparse y Parse
+
+(define unparse
+  (lambda (exp)
+    (cases sumaAnidada exp
+      (valorType (x) (list 'valor x))
+      (sumaType (x body)
+                (list 'sumaAnidada (list 'valor x) (unparse body))))))
+
+
+(define parse
+  (lambda (dato)
+    (cond
+      [(number? (valor->numero dato)) (valorType (valor->numero dato))]
+      [(pair? dato)
+       (if  (eqv? (car dato) 'sumaAnidada)
+           (sumaType (cadr(valor->numero dato))
+                     (parse (caddr dato)))
+           ('invalido)
+           )]
+      (else 'invalido))))
+
+;codigo cliente
+
+(define suma->valDer
+  (lambda (l)
+    (caddr l)))
+
+(define suma->valIzq
+  (lambda (l)
+    (cadr l)))
+
+(define valor->numero
+  (lambda (l)
+    (cadr l)))
+
+(define valor?
+  (lambda (l)
+    (eqv? (car l) 'valor)))
+
+(define suma-anidada
+  (lambda (exp)
+    (if (valor? exp)
+        (valor->numero exp)
+        (+ (suma-anidada (suma->valIzq exp))
+           (suma-anidada (suma->valDer exp))
+           )
+        )
+    )
+  )
+
+(define a
+  (sumaType 4        
+        (sumaType 5 (valorType 6) )
+        ))
+
+(define listaA '(sumaAnidada (valor 4) (sumaAnidada (valor 5) (valor 6))))
+
+(unparse a)
+(parse listaA)
+(suma-anidada (unparse a))
+
+;-------------------------------------------------------------------------------
+;punto 3
+
 
