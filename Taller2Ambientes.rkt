e2cc6cd85fa92fc7c3f37c9feceeef59d3cdfa15
Juanse <jsebastian.va@gmail.com>
2019-06-22 01:30:42 -0500

    Se termina punto 3 con datatype y sus Parse-Unparse, se comentay colocan varias pruebas en el programa, documentando un poco

--- Taller2Ambientes.rkt
+++ Taller2Ambientes.rkt
@@ -147,7 +147,8 @@
 (suma-anidada (unparse a))
 
 ;-------------------------------------------------------------------------------
-;punto 3
+;Punto 3
+
 ;----------------------------------------------
 ;LISTAS
 ;----------------------------------------------
@@ -176,12 +177,8 @@
 (define top
   (lambda (stack)
     (cond
-      [(empty-stack? stack) '()]
+      [(empty-stack? stack) "The stack is null"]
       [else (car stack)])))
-
-
-
-
 
 ;Observador empty-satck? Predicado que se encarga de preguntar
 ;si la pila está vacía 
@@ -191,10 +188,11 @@
         #t #f))) 
 
 
-;Pruebas
+;Definicion stack1 Generico
 (define stack1 (list 3 3 3 4 52 1 'empty-stack))
 (push 'x stack1)
 
+;Definicion stackPush por medio del constructor push
 (define stackPush
   (push 'a
         (push 'b
@@ -202,9 +200,11 @@
                     (push 'd
                           (empty-stack))))))
 
-(pop stackPush)
-(top stackPush)
-(empty-stack? stack1)
+;Pruebas
+(pop stackPush); Debe retornar (b c d empty-stack)
+(top stackPush); Debe retornar 'a
+(empty-stack? stack1); Debe retorna #f
+(top (empty-stack)); Debe retornar "The stack is Null"
 
 
 ;----------------------------------------------
@@ -227,8 +227,9 @@
   (lambda ()
     (lambda (id)
       (cond
-        [(or (eqv? id 1) (eqv? id 2))'()]
-        [(eqv? id 3) 'error]))))
+        [(eqv? id 1)'()]
+        [(eqv? id 2) "The stack is null"]
+        [(eqv? id 3) #t]))))
 
 
 (define pushP
@@ -245,6 +246,116 @@
                 (pushP 'c
                        (pushP 'd
                               (empty-stackP))))))
+
+;Pruebas
+(empty-stackP? (empty-stackP)); Debe retornar true
+(popP (empty-stackP)); Debe retonar '()
+(topP (empty-stackP)); Debe retornar "The stack is null"
+(topP stackPushP); Debe retornar 'a
+(topP (pushP 'x stackPushP)); Debe retornar 'x
+(topP (popP stackPushP)); Debe retornar 'b
+
+
+;----------------------------------------------
+;DATATYPES
+;----------------------------------------------
+;Defino tipo de dato stack por Datatype con los constructores
+;empty-stackType y pushType
+(define-datatype stackType stackType?
+  (empty-stackType)
+  (pushType (element scheme-value?)
+            (stackBody stackType?))
+  )
+
+(define scheme-value? (lambda (v) #t))
+
+;Funciones de observadores, en este caso son extractores
+(define topType
+  (lambda (stack)
+    (cases stackType stack
+      (empty-stackType ()
+                       "The stack is null")
+      (pushType (element stackBody)
+                element)
+      )))
+
+(define popType
+  (lambda (stack)
+    (cases stackType stack
+      (empty-stackType ()
+                       (stack))
+      (pushType (element stackBody)
+                stackBody)
+      )))
+
+(define empty-stackType?
+  (lambda (stack)
+    (cases stackType stack
+      (empty-stackType ()
+                       #t)
+      (pushType (element stackBody)
+                #f)
+      )))
+
+;Definicion de stacks por medio de Datatypes, para algunas pruebas
+(define stackType1
+  (pushType 2
+            (pushType 'y
+                      (pushType 3
+                                (pushType 'w
+                                          (empty-stackType))))))
+(define stackType2
+  (pushType 'x
+            (empty-stackType)))
+
+;Pruebas
+(popType stackType1); Debe retornar #(struct:pushType y #(struct:pushType 3 #(struct:pushType w #(struct:empty-stackType))))
+(+ 2 (topType stackType1)); Debe retornar 4
+(topType (popType stackType1)); Debe retornar 'y
+(topType (popType stackType2)); Debe retornar "The stack is null"
+(empty-stackType? (popType stackType2));Debe retornar #t
+(empty-stackType? stackType1); Debe retornar #f
+
+
+;UNPARSE Y PARSE
+
+(define unparseStack
+  (lambda (stack)
+    (cases stackType stack
+      (empty-stackType ()
+                       '(emptyStackList))
+      (pushType (element stackBody)
+                (list 'pushList element (unparseStack stackBody)))
+      )
+    )
+  )
+
+(define parseStack
+  (lambda (dato)
+    (if (pair? dato)
+        (cond
+          [(eqv? (car dato) 'emptyStackList) (empty-stackType)]
+          [(eqv? (car dato) 'pushList)(pushType
+                                       (dato->elemento dato)
+                                       (parseStack (caddr dato)))]
+          [else 'Invalido])
+        'Invalido)))
+
+(define dato->elemento
+  (lambda (list)
+    (cadr list)))
+
+
+(define stackPrueba (list 'pushList 2 '(emptyStackList)))
+
+;Pruebas
+(unparseStack stackType1); Debe retornar (pushList 2 (pushList y (pushList 3 (pushList w (emptyStackList)))))
+(unparseStack stackType2); Debe retornar (pushList x (emptyStackList))
+(parseStack(unparseStack stackType2));Debe retornar #(struct:pushType x #(struct:empty-stackType)) 
+(parseStack stackPrueba); Debe retornar #(struct:pushType 2 #(struct:empty-stackType))
+(parseStack stackType1); Debe retornar Invalido
+
+
 
 ;-------------------------------------------------------------------------------
 ;punto 4
@@ -295,9 +406,7 @@
     (cond [(or (empty-tree? tree)
                (leaf-tree? tree)) #t]
           [else (and (validador-orden (extract-izq tree)) 
-                     (validador-orden (extract-der tree))
-                     ()
-                     ())]
+                     (validador-orden (extract-der tree)))]
           )
     )
   )
