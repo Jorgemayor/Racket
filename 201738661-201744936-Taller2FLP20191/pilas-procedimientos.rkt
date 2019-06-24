c2f9a67312714d5d2afe55f4d4f0b0e7c5629361
Jorgemayor <40550036+Jorgemayor@users.noreply.github.com>
2019-06-24 17:04:45 -0500

    Create pilas-procedimientos.rkt

---
+++ 201738661-201744936-Taller2FLP20191/pilas-procedimientos.rkt
@@ -0,0 +1,49 @@
+;Encabezado
+
+;----------------------------------------------
+;PROCEDIMIENTOS
+;----------------------------------------------
+
+(define popP
+  (lambda (stack)
+    (stack 1)))
+
+(define topP
+  (lambda (stack)
+    (stack 2)))
+
+(define empty-stackP?
+  (lambda (stack)
+    (stack 3)))
+
+(define empty-stackP
+  (lambda ()
+    (lambda (id)
+      (cond
+        [(eqv? id 1)'()]
+        [(eqv? id 2) "The stack is null"]
+        [(eqv? id 3) #t]))))
+
+
+(define pushP
+  (lambda (element stack)
+    (lambda (id)
+      (cond
+        [(eqv? id 1) stack]
+        [(eqv? id 2) element]
+        [(eqv? id 3) (empty-stackP id)]))))
+
+(define stackPushP
+  (pushP 'a
+         (pushP 'b
+                (pushP 'c
+                       (pushP 'd
+                              (empty-stackP))))))
+
+;Pruebas
+(empty-stackP? (empty-stackP)); Debe retornar true
+(popP (empty-stackP)); Debe retonar '()
+(topP (empty-stackP)); Debe retornar "The stack is null"
+(topP stackPushP); Debe retornar 'a
+(topP (pushP 'x stackPushP)); Debe retornar 'x
+(topP (popP stackPushP)); Debe retornar 'b
