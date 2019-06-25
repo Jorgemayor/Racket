bc6b99f862c71fcaf05db8975eed15e73ee2569e
Jorgemayor <40550036+Jorgemayor@users.noreply.github.com>
2019-06-24 23:55:43 -0500

    Update pilas-listas.rkt

--- 201738661-201744936-Taller2FLP20191/pilas-listas.rkt
+++ 201738661-201744936-Taller2FLP20191/pilas-listas.rkt
@@ -1,60 +1,109 @@
 #lang eopl
 
-;Encabezado
+; Taller 2 Fundamentos de lenguaje de programacion
+; 
+; pilas-listas.rkt
+; 
+; Desarrolladores:
+; 
+; Jorge Eduardo Mayor Fernandez
+; Codigo: 201738661
+; 
+; Juan Sebastian Velasquez Acevedo
+; Codigo: 201744936
 
 ;----------------------------------------------
-;LISTAS
+;GRAMATIC
 ;----------------------------------------------
-;Definicion pila vacía
+
+; <stack> ::= (empty-stack)
+;         ::=(push <scheme-value> <stack>)
+
+;----------------------------------------------
+;LISTS
+;----------------------------------------------
+
+;empty-stack: {void}
+;             -> {list}
+;Purpose:
+;Definicion de pila vacía
 (define empty-stack
   (lambda () (list 'empty-stack)))
+;Prueba:
+(empty-stack)
 
+;Definicion stack Generico
+(define stack1 (list 3 3 3 4 52 1 'empty-stack))
+(push 'x stack1)
 
-;Push=>Insertar elemento en una pila
-
-;Insertarlo al inicio
+;push: element {any}
+;      stack {list}
+;      -> {list}
+;Purpose:
+;Inserts an element at the begginning of the given stack
 (define push
   (lambda (element stack)
     (cond
       [(empty-stack? stack) (cons element (empty-stack))]
       [else (cons element stack)])))
 
-;Pop=>Retira el elemento superior de la pila
-(define pop
-  (lambda (stack)
-    (cond
-      [(empty-stack? stack) '()]
-      [else (cdr stack)])))
+;Definiciones de stacks por medio del constructor push (Pruebas)
 
-;Top=>Devuelve el elemento superior de la pila sin retirarlo
-(define top
-  (lambda (stack)
-    (cond
-      [(empty-stack? stack) "The stack is null"]
-      [else (car stack)])))
-
-;Observador empty-satck? Predicado que se encarga de preguntar
-;si la pila está vacía 
-(define empty-stack?
-  (lambda (stack)
-    (if (eqv? (car stack) 'empty-stack)
-        #t #f))) 
-
-
-;Definicion stack1 Generico
-(define stack1 (list 3 3 3 4 52 1 'empty-stack))
-(push 'x stack1)
-
-;Definicion stackPush por medio del constructor push
-(define stackPush
+(define stackPush1
   (push 'a
         (push 'b
               (push 'c
                     (push 'd
                           (empty-stack))))))
 
+(define stackPush2
+  (push 1
+        (push 2
+              (push 3
+                    (empty-stack)))))
+
+
+;pop: stack {list}
+;      -> {list}
+;Purpose:
+;Remove an element at the begginning of the stack
+(define pop
+  (lambda (stack)
+    (cond
+      [(empty-stack? stack) '()]
+      [else (cdr stack)])))
+
+;Pruebas:
+(pop stackPush1)
+(pop stackPush1)
+
+;top: stack {list}
+;      -> {any}
+;Purpose:
+;Shows the element at the top of the given stack, without removing it.
+(define top
+  (lambda (stack)
+    (cond
+      [(empty-stack? stack) "The stack is null"]
+      [else (car stack)])))
+
+;Pruebas:
+(top (empty-stack))
+(top stackPush1)
+(top stackPush2)
+
+
+;top: stack {list}
+;      -> {boolean}
+;Purpose:
+;Evaluates if the stack is empty
+;Returns true if it is, false otherwise.
+(define empty-stack?
+  (lambda (stack)
+    (if (eqv? (car stack) 'empty-stack)
+        #t #f))) 
+
 ;Pruebas
-(pop stackPush); Debe retornar (b c d empty-stack)
-(top stackPush); Debe retornar 'a
-(empty-stack? stack1); Debe retorna #f
-(top (empty-stack)); Debe retornar "The stack is Null"
+(empty-stack? stack1)
+(empty-stack? (empty-stack))
+
